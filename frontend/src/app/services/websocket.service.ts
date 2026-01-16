df1ec6d7ce4ea0797a5834f54cf28d54825463ad
zwwxxxxx <126754383+ogkunta666@users.noreply.github.com>
2026-01-16 08:52:23 +0100

    Add WebSocket service

--- frontend/src/app/services/websocket.service.ts
+++ frontend/src/app/services/websocket.service.ts
@@ -1,1 +1,34 @@
-(complete WebSocket service)
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+  private socket: Socket;
+
+  constructor() {
+    this.socket = io('http://localhost:6001', {
+      transports: ['websocket']
+    });
+  }
+
+  listen(eventName: string): Observable<any> {
+    return new Observable((subscriber) => {
+      this.socket.on(eventName, (data: any) => {
+        subscriber.next(data);
+      });
+    });
+  }
+
+  emit(eventName: string, data: any) {
+    this.socket.emit(eventName, data);
+  }
+
+  disconnect() {
+    if (this.socket) {
+      this.socket.disconnect();
+    }
+  }
+}
