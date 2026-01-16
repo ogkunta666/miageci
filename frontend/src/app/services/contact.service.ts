90948a4427b0276888865e4cf013e2dbc1c52bd4
zwwxxxxx <126754383+ogkunta666@users.noreply.github.com>
2026-01-16 09:00:44 +0100

    Add Contact service

--- frontend/src/app/services/contact.service.ts
+++ frontend/src/app/services/contact.service.ts
@@ -1,1 +1,21 @@
-(complete Contact service)
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ContactMessage } from '../models/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+  private apiUrl = 'http://localhost:8000/api/contact';
+
+  constructor(private http: HttpClient) {}
+
+  sendMessage(message: ContactMessage): Observable<ContactMessage> {
+    return this.http.post<ContactMessage>(this.apiUrl, message);
+  }
+
+  getMessages(): Observable<ContactMessage[]> {
+    return this.http.get<ContactMessage[]>(this.apiUrl);
+  }
+}
